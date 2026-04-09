19dd27c4f7e8bee8b85d6d6da84bda39f32bc078
swasti101 <swastij64@gmail.com>
2026-04-09 22:29:14 +0530

    Move canonical grader files to tasks package

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,13 +1,8 @@
-"""Task and grading compatibility package.
+"""Task and grading package."""
 
-This package mirrors the scaffold-style project structure expected by some
-OpenEnv validators while re-exporting the implementation that lives under
-``server/``.
-"""
-
-from server.baseline import get_baseline_summary
-from server.grader import grade_request, normalize_score, weighted_cost
-from server.tasks import TASKS, TASK_ORDER, TaskConfig, get_task, list_task_summaries
+from tasks.baseline import get_baseline_summary
+from tasks.grader import grade_request, normalize_score, weighted_cost
+from tasks.tasks import TASKS, TASK_ORDER, TaskConfig, get_task, list_task_summaries
 
 __all__ = [
     "TASKS",
